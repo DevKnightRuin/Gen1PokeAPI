2a9b31579b733305287393624d4b57d8f6fdc2b7
nick <devknightruin@outlook.com>
2024-01-28 18:04:23 -0600

    Updated data files to not had redudant names on the return. ie pokemon.pokemon to get access to the data

--- data/typeweakness.js
+++ data/typeweakness.js
@@ -1,53 +1,50 @@
-const weaknessChart = {
-    'normal':{
-        'weakness':['fighting']
-    },
-    'normal':{
-        'weakness': ['fighting']
-    },
-    'fire':{
-        'weakness':['water',  'ground',  'rock']
-    },
-    'water':{
-        'weakness':['electric',  'grass']
-    },
-    'grass':{
-        'weakness':['fire',  'ice',  'poison', 'flying', 'bug']
-    },
-    'electric':{
-        'weakness':['ground']
-    },
-    'ice':{
-        'weakness':['fire',  'fighting',  'rock']
-    },
-    'fighting':{
-        'weakness':['flying','psychic'],
-    },
-    'poison':{
-        'weakness':['ground',  'psychic', 'bug']
-    },
-    'ground':{
-        'weakness':['water',  'grass',  'ice']
-    },
-    'flying':{
-        'weakness':['electric',  'ice',  'rock']
-    },    
-    'psychic':{
-        'weakness':['bug'],
-    },
-    'bug':{
-        'weakness':['fire', 'poison', 'flying', 'rock']
-    },
-    'rock':{
-        'weakness':['water',  'grass',  'fighting', 'ground']
-    },
-    'ghost':{
-        'weakness':['ghost']
-    },
-    'dragon':{
-        'weakness':['ice',  'dragon']
-    },
-}
-module.exports={weaknessChart}
-
-
+module.exports = {
+	normal: {
+		weakness: ["fighting"],
+	},
+	normal: {
+		weakness: ["fighting"],
+	},
+	fire: {
+		weakness: ["water", "ground", "rock"],
+	},
+	water: {
+		weakness: ["electric", "grass"],
+	},
+	grass: {
+		weakness: ["fire", "ice", "poison", "flying", "bug"],
+	},
+	electric: {
+		weakness: ["ground"],
+	},
+	ice: {
+		weakness: ["fire", "fighting", "rock"],
+	},
+	fighting: {
+		weakness: ["flying", "psychic"],
+	},
+	poison: {
+		weakness: ["ground", "psychic", "bug"],
+	},
+	ground: {
+		weakness: ["water", "grass", "ice"],
+	},
+	flying: {
+		weakness: ["electric", "ice", "rock"],
+	},
+	psychic: {
+		weakness: ["bug"],
+	},
+	bug: {
+		weakness: ["fire", "poison", "flying", "rock"],
+	},
+	rock: {
+		weakness: ["water", "grass", "fighting", "ground"],
+	},
+	ghost: {
+		weakness: ["ghost"],
+	},
+	dragon: {
+		weakness: ["ice", "dragon"],
+	},
+};
--- server.js
+++ server.js
@@ -1,77 +1,69 @@
-const { response } = require('express');
-const express = require('express')
-const appDir = require('path')
-const cors = require('cors')
+const { response } = require("express");
+const express = require("express");
+const appDir = require("path");
+const cors = require("cors");
 const app = express();
 const PORT = 8080;
 
-app.use(cors())
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true}))
-app.use(express.json())
+app.use(cors());
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-console.log('yippu')
+console.log("yippu");
 
-const pokemon = require('./data/pokemonlist.js')
-const weaknessChart = require('./data/typeweakness.js');
-const { status } = require('express/lib/response.js');
+const pokemon = require("./data/pokemonlist.js");
+const weaknessChart = require("./data/typeweakness.js");
+const { status } = require("express/lib/response.js");
 
 //create some data to send back
 
-app.get('/', (req, res) => {
-    res.sendFile("index.html")
-})
+app.get("/", (req, res) => {
+	res.sendFile("index.html");
+});
 
 //allows render to verify the server status
-app.get('/checkme', (req, res) => {
-    const options = { status: 200, statusText: 'ok'}
-    res.sendStatus(200)
-})
+app.get("/checkme", (req, res) => {
+	const options = { status: 200, statusText: "ok" };
+	res.sendStatus(200);
+});
 
+app.get("/api/pokemon", (req, res) => {
+	res.json({ pokemon });
+});
 
-app.get('/api/pokemon', (req, res) => {
-    res.json({pokemon})
-})
+app.get("/api/weakness", (req, res) => {
+	res.json({ weaknessChart });
+});
 
-app.get('/api/weakness', (req, res) => {
-    res.json({weaknessChart})
-})
+app.get("/api/pokemon/:name", (req, res) => {
+	let pokename = convertString(req.params.name);
+	console.log(pokename);
+	if (pokemon.pokemon[pokename]) {
+		res.json(pokemon.pokemon[pokename]);
+	} else {
+		res.json("invalid request - check spelling for errors");
+	}
+});
 
+app.get("/api/pokemon/image/:name", (req, res) => {
+	let pokename = convertString(req.params.name);
+	console.log(pokename);
+	if (pokemon.pokemon[pokename]) {
+		res.json(pokemon.pokemon[pokename].sprites.front);
+	} else {
+		res.json("invalid request - check spelling for errors");
+	}
+});
 
-app.get('/api/pokemon/:name', (req, res) => {
+app.listen(process.env.PORT || PORT, function () {
+	console.log("listening in on " + PORT);
+});
 
-       let pokename = convertString(req.params.name)
-       console.log(pokename)
-       if(pokemon.pokemon[pokename]){
-           res.json(pokemon.pokemon[pokename])
-        }
-        else{
-         res.json('invalid request - check spelling for errors')
-        }
-})
-
-app.get('/api/pokemon/image/:name', (req, res) =>{
-   
-    let pokename = convertString(req.params.name)
-    console.log(pokename)
-    if(pokemon.pokemon[pokename]){
-        res.json(pokemon.pokemon[pokename].sprites.front)
-     }
-     else{
-      res.json('invalid request - check spelling for errors')
-     }
-})
-
-app.listen(process.env.PORT || PORT, function(){
-    console.log('listening in')
-})
-
-
-
-function convertString(str){
-    //format string to prevent any errors with capitilization
-    str.toLocaleLowerCase().trim()
-    let temp = str.split('')
-    temp[0] = str[0].toUpperCase()
-    return temp.join('');
+function convertString(str) {
+	//format string to prevent any errors with capitilization
+	str.toLocaleLowerCase().trim();
+	let temp = str.split("");
+	temp[0] = str[0].toUpperCase();
+	return temp.join("");
 }
